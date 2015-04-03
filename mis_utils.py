ce1ed12f4c184ee447efbd27e81ec5eacf6b1229
asing <asing@spearmint.brightprocess.com>
2015-04-03 12:53:48 +0100

    Basics in place

--- mis_utils.py
+++ mis_utils.py
@@ -6,8 +6,82 @@ import yaml
 import paramiko
 import socket
 import hashlib
+import sys
+import psutil
+import pwd
 
-def execute_action(args, task):
+class logunit ():
+   """
+   A class for all output from makeitso
+   It allows you to be more or less verbose
+   and provides the option to specify an alternate logfile
+   """
+
+   def __init__(self, quiet=False, verbose=False, logfile='syslog'):
+     self.quiet = quiet
+     self.verbose = verbose
+     self.logfile = logfile
+     self.process = sys.argv[0]
+
+   def log(self, level='info', message='null message'):
+     to_syslog = False
+     to_console = False
+
+     if level =='crit':
+       loglevel = syslog.LOG_ERR
+       to_syslog = True
+       to_console = True
+     if level == 'warn':
+       to_syslog = True
+       to_console = True
+       loglevel = syslog.LOG_WARNING
+     if level == 'info':
+       to_console = True
+       loglevel = syslog.LOG_INFO
+     if level == 'debug':
+       loglevel = syslog.LOG_DEBUG
+
+     # Print to logfile?
+     if self.verbose == True:
+       to_syslog = True
+       to_console = True
+
+     if self.quiet == True:
+       to_syslog = False
+       to_console = False
+
+
+     if to_syslog == True:
+       syslog.syslog(loglevel, message)
+
+     if to_console == True:
+       print "%s: %s" % (level.upper(), message)
+      
+     if level == 'crit':
+       exit(1)
+
+def authorize(mislog, sig_valid, run_local, user_valid):
+  # Allow execution for the following criteria:
+
+  mislog.log('info','Checking authorization')
+  mislog.log('debug',('sig match?: %s' % sig_valid))
+  mislog.log('debug',('user match?: %s' % user_valid))
+  mislog.log('debug',('local run?: %s' % run_local))
+
+  # The checksum matches and the user is valid
+  if sig_valid == True and user_valid == True:
+    return True
+
+  # No checksum was provided (nohashcheck), but
+  # - the utility was executed locally
+  # - the user is valid
+  if sig_valid == False and user_valid == True and run_local == True:
+    return True
+
+  mislog.log('crit','You are not authorised to execute this action')
+  return False
+
+def execute_action(mislog, task):
   jobuser = task['user']
   jobs = task['command']
   servers = task['servers']
@@ -15,112 +89,67 @@ def execute_action(args, task):
   failcontinue = task['failcontinue']
 
   for server in servers:
-    logger(args, 'info', 'Connecting to: '+server)
-    addr = socket.gethostbyname(server)
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(addr,username='makeitso', key_filename='/etc/makeitso/deploy')
+    mislog.log('info', 'Connecting to: '+server)
+    try:
+      addr = socket.gethostbyname(server)
+    except:
+      mislog.log('crit','DNS lookup failure for %s' % server)
+    try:
+      ssh = paramiko.SSHClient()
+      ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+      ssh.connect(addr,username='makeitso')
+    except:
+      mislog.log('crit','Could not connect to %s' % server)
     try:
       channel = ssh.invoke_shell()
       channel.set_combine_stderr(True)
       stdin = channel.makefile('wb')
       stdout = channel.makefile('rb')
     except:
-      logger(args,'critical','Could not run remote command on '+server)
+      mislog.log('critical','Could not run remote command on '+server)
     for comm in task['command']:
       command="sudo su - %s\n" % jobuser
       command+="cd %s\n" % cwd
       command+=comm
       command+="\n exit\n exit\n"
-      logger(args, 'debug', 'Executing: '+command)
+      mislog.log('debug', 'Executing: '+command)
       try:
         stdin.write(command)
         outstring=stdout.read()
-        logger(args, 'debug', 'Console output: '+outstring)
+        mislog.log('debug', 'Console output: '+outstring)
       except:
         if failcontinue == False:
           stdout.close()
           stdin.close()
           ssh.close
-          logger(args,'critical',('Server %s could not finish: %s' % ( server, comm)))
+          mislog.log('critical',('Server %s could not finish: %s' % ( server, comm)))
         else:
-          logger(args,'warn',('Server %s could not finish: %s' % ( server, comm)))
+          mislog.log('warn',('Server %s could not finish: %s' % ( server, comm)))
     stdout.close()
     stdin.close()
     ssh.close
   return
 
-def validate_everything(args, yaml_object):
-  validate_environment(args, yaml_object)
-  validate_user(args, yaml_object)
-  validate_action(args, yaml_object)
+def get_local():
+  parentpid = psutil.Process().ppid()
+  grandparentpid = psutil.Process(parentpid).ppid()
+  req_exe = psutil.Process(grandparentpid).name()
+  if req_exe == 'makeitso':
+    return True
+  else:
+    return False
 
-def validate_user(args, yaml_object):
-  print 'null check'
-
-def validate_action(args, yaml_object):
-  environment = args.environment
-  action = args.action
-  actionlist = []
-  for keys in yaml_object[environment]:
-    actionlist.append(keys['action'])
-  if action not in actionlist:
-    logger(args,'critical',('Could not find action %s' % action ))
-      
-def validate_environment(args, yaml_object):
-  environment = args.environment
-  try:
-    yaml_object[environment]
-    logger(args, 'info', 'Environment: '+environment)
-  except:
-    logger(args, 'critical', 'Can\'t find the environment: '+environment)
-
-### validate_strings: Ensure all required yaml elements are present
-def validate_strings(args, item, listit):
-  for listitem in listit:
-    try:
-      item[listitem]
-    except:
-      logger(args, 'critical', 'YAML definition requires these items: '+str(listit))
-
-def default_items(args, item, mydict):
-  for key, value in mydict.iteritems():
-    try:
-      item[key]
-    except:
-      logger(args, 'debug', 'Defaulting '+key+' to '+str(value))
-      item[key]=value
-  return(item)
-
-
-### get_roles:Get roles attributed to the user
-def get_roles(args, user,  yaml_object):
-  environment = args.environment
-  privlist = []
-
-  for item in yaml_object['environments'][environment]:
-    if item['type'] == 'role':
-      for itemuser in item['users']:
-        if itemuser == user:
-          for roleitem in yaml_object['roles'][item['name']]:
-            privlist.append(roleitem)
-  
-  return(privlist)
-
-### get_tasks: Find all tasks for an environment and action
-def get_task(action, yaml_object):
-
-  for item in yaml_object[makeitso]:
-    try:
-      item['action']
-      return(item)
-    except:
-      pass
-
-  logger(args, 'critical', 'The action {} is not defined for environment {}.',action,environment)
+def get_user(mislog, action_yaml):
+  parentpid = psutil.Process().ppid()
+  grandparentpid = psutil.Process(parentpid).ppid()
+  req_user = psutil.Process(grandparentpid).username()
+  for user in action_yaml['allowed']:
+    if user == req_user:
+      return True
+  return False
 
 ### get_yaml: iterate over all yaml files ###
-def get_yaml( filespec ):
+def get_yaml(mislog, filespec):
 
   yaml_object = {}
   yaml_file = open(filespec)
@@ -128,71 +157,100 @@ def get_yaml( filespec ):
   try:
     yaml_object.update(yaml.load(yaml_file))
   except:
-    return false
+    mislog.log('crit','Could not load the yaml file. Please check syntax.')
+    return False
 
   yaml_file.close()
 
   return yaml_object
 
 ### Get the signature of the file provided
-def compare_sig(filespec,hashcheck):
+def compare_sig(mislog, filespec, hashcheck):
   myfile = open(filespec)
-  sig = hashlib.sha256(myfile).digest()
+  sig = hashlib.sha256(myfile.read()).digest()
   myfile.close()
   if sig == hashcheck:
     return True
   else:
     return False
 
-### logger: Logging handler ###
-def logger( args, level, message ):
+### get_action: Extract the relevant action portion of the yaml
+def get_action(mislog,env_yaml,action):
+  try:
+    action_yaml = env_yaml['makeitso'].action
+  except:
+    mislog.log('crit','The requested action is not defined: %s' % action)
+  return action_yaml
 
-  if level =='critical':
-    loglevel = syslog.LOG_ERR
-    score=4
-  if level == 'warn':
-    loglevel = syslog.LOG_WARNING
-    score=3
-  if level == 'info':
-    loglevel = syslog.LOG_INFO
-    score=3
-  if level == 'debug':
-    loglevel = syslog.LOG_DEBUG
-    score=1
+### Show_list: list the actions available for this environment
+# Along with any parameters
+def show_list(mislog, args, env_yaml):
+  if args.action == 'list':
+    print "\n-----------------"
+    for action in env_yaml['makeitso']:
+      extra = ""
+      try:
+        for key in env_yaml['makeitso'][action]['parameters']:
+          extra += "%s[=%s] " % (key, env_yaml['makeitso'][action]['parameters'][key])
+      except:
+        pass
 
-  threshold = 3
+      print "task> %s %s" % (action, extra)
+    print "-----------------"
+    print ""
+    exit(0)
+  else:
+    return
 
-  if args.quiet == True:
-    threshold=5
-  if args.verbose == True:
-    threshold=1
+### sub_params: Perform substitutions of the passed parameters
+### into the execution sections
+def sub_params(mislog, env_yaml, params):
+    final_params = {}
 
-  if (threshold-1) <= score:
-    syslog.syslog(syslog.LOG_ERR, message)
-  if threshold <= score:
-    print level+': '+message
+    # Set defaults from the yaml
+    for param in env_yaml['parameters']:
+        final_params[param] = env_yaml['parameters'][param]
+        mislog.log('debug',"Default parameter %s is %s" % (param, env_yaml['parameters'][param]))
 
-  if level == 'critical':
-    exit(1)
+    # Override with the cl args
+    for param_string in params.split(','):
+        param, value = params.split('=')
+        final_params[param] = value
+        mislog.log('debug',"Given parameter %s is %s" % (param, value))
 
+    for index, command in enumerate(env_yaml['command']):
+      mislog.log('debug',"Substitution yaml: %s" % final_params)
+      for param, value in final_params.iteritems():
+        command = command.replace(("__%s__" % param),value)
+      env_yaml['command'][index] = command
+    return env_yaml
 
 ### parse_validate: Ensure we are about to do something valid
-def parse_validate(args, yaml):
+def parse_validate(mislog, args, yaml):
 
   # Do we have an environment
   try:
     args.environment
   except:
-    logger('crit','Parser failed for environment: %s' % args['environment'])
+    mislog.log('crit','Parser failed for environment: %s' % args['environment'])
 
   # Is our action valid for our environment
-  actions=get_actions(yaml)
   try:
-    actions[args.action]
+    yaml['makeitso'][args.action]
   except:
-    logger('crit','Parser failed for action: %s' % args.action)
+    if args.action == 'list':
+      pass
+    else:
+      mislog.log('crit','Parser failed. Cant find action %s' % args.action)
 
-  return true
+  # Do our parameters exist in our job
+  if args.params :
+    for param_string in args.params.split(','):
+      param, value = args.params.split('=')
+      if param not in yaml['makeitso'][args.action]['parameters']:
+          mislog.log('crit','Parser failed. Your parameter is not defined in the job')
+    
+
 
 ### arg_parser: Handle command-line arguments
 def arg_parser():
